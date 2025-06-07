5426ce33d5a2ede2c2262674b76c5f22ec539df7
ArpanUpreti1 <arpanuprety50@gmail.com>
2025-06-07 16:43:34 +0545

    completed a 3D website made changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,31 +4,6 @@ import './index.css'
 import React from 'react'
 import App from './App.jsx'
 
-import * as Sentry from "@sentry/react";
-
-Sentry.init({
-  dsn: "https://2b94938688d659b57cb8c5a53cd1ddba@o4509455763243008.ingest.us.sentry.io/4509455766126592",
-  // Setting this option to true will send default PII data to Sentry.
-  // For example, automatic IP address collection on events
-  sendDefaultPii: true,
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-    Sentry.reactRouterV6BrowserTracingIntegration({
-      useEffect: React.useEffect,
-    })
-  ],
-
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
-
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
